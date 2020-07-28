b2848f74ffc86f6fda114d89087a4706cc8da59b
france1988 <france1988@naver.com>
2020-07-28 13:16:50 +0900

    20200728_PS Finish

--- src/com/lombre/tree_graph/GraphTest.java
+++ src/com/lombre/tree_graph/GraphTest.java
@@ -134,7 +134,15 @@ class Graph {
 
 
 
-
+/**
+ * 		0
+ * 	  /
+ * 	 1 -- 3  	7
+ *	 |  / | \  /
+ *	 | /  |  5
+ *   2 -- 4   \
+ *             6 - 8
+ */
 public class GraphTest {
 	public static void main(String[] args) {
 		Graph g = new Graph(9);
@@ -149,9 +157,13 @@ public class GraphTest {
 		g.addEdge(5, 7);
 		g.addEdge(6, 8);
 
-		//g.dfs(); // dfs 실행 (노드=0)
-		g.dfsR();  // dfs 재귀 실행 (노드=0)
-		//g.bfs(); // bfs 실행 (노드=0)
+		//g.dfs(); // dfs 실행(0)  [0 1 3 5 7 6 8 4 2]
+		//g.dfsR();  // dfs 재귀 실행(0) [0 1 2 4 3 5 6 8 7]
+		//g.bfs(); // bfs 실행(0) [0 1 2 3 4 5 6 7 8]
+
+		//g.dfs(3); // dfs 실행(3)  [3 5 7 6 8 4 2 1 0]
+		//g.dfsR(3);  // dfs 재귀 실행(3) [3 1 0 2 4 5 6 8 7]
+		//g.bfs(3); // bfs 실행(3) [3 1 2 4 5 0 6 7 8]
 
 
 	}
